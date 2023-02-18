c98dad1b72660a9e5732f805d41e5ebe7657d417
Kirill <shkolenko02-03-2004@mail.ru>
2023-02-18 13:37:42 +0300

    Do adaptive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,28 +4,28 @@
     <h1 class="text-center team__header">Наша команда</h1>
     <div class="row">
       <div class="row">
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/office_1.png" alt="">
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/office_2.png" alt="">
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/office_3.png" alt="">
         </div>
       </div>
     </div>
 
-    <h1 class="text-center team__header">Наша офис</h1>
+    <h1 class="text-center team__header">Наш офис</h1>
     <div class="row">
       <div class="row">
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/team_1.png" alt="">
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/team_2.png" alt="">
         </div>
-        <div class="col-4">
+        <div class="col-lg-4 col-md-12">
           <img class="office_image" src="../assets/team_3.png" alt="">
         </div>
       </div>
@@ -52,6 +52,7 @@ export default {
 }
 .office_image{
   height: 300px;
+  margin-top: 20px;
   width: 100%;
 }
 </style>
